427478f6a3535d784fe582471f6bbd6cb153eaf6
Aloys Aboge Jr <76847396+AbogeJr@users.noreply.github.com>
2022-02-02 22:38:14 +0300

    added layout && BS styling

--- myapp.py
+++ myapp.py
@@ -13,16 +13,34 @@ posts = [
         'title': 'Post Two',
         'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et ipsa ex itaque? Explicabo adipisci amet corrupti, nobis, ipsa, nam minus tempore aspernatur alias neque earum laboriosam consequuntur excepturi nisi expedita!',
         'date_posted': 'May 8, 2021'
+    },
+    {
+        'author':'Jim Halpert',
+        'title': 'Post Three',
+        'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et ipsa ex itaque? Explicabo adipisci amet corrupti, nobis, ipsa, nam minus tempore aspernatur alias neque earum laboriosam consequuntur excepturi nisi expedita!',
+        'date_posted': 'May 8, 2021'
+    },
+    {
+        'author':'Pam Beasley',
+        'title': 'Post Four',
+        'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et ipsa ex itaque? Explicabo adipisci amet corrupti, nobis, ipsa, nam minus tempore aspernatur alias neque earum laboriosam consequuntur excepturi nisi expedita!',
+        'date_posted': 'May 8, 2021'
+    },
+    {
+        'author':'Michael Scott',
+        'title': 'Post Five',
+        'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et ipsa ex itaque? Explicabo adipisci amet corrupti, nobis, ipsa, nam minus tempore aspernatur alias neque earum laboriosam consequuntur excepturi nisi expedita!',
+        'date_posted': 'May 8, 2021'
     }
 ]
 
 @app.route("/")
 def hello_world():
-    return render_template('home.html', posts=posts)
+    return render_template('home.html', posts=posts, title="Home")
 
 @app.route("/about")
 def about():
-    return render_template('about.html')    
+    return render_template('about.html', title="About")    
 
 if __name__ == '__main__':
     app.run(debug=True)
