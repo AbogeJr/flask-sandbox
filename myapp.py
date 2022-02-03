1fe0c17252323a135893d3f5dd3afd20175aa67a
Aloys Aboge Jr <76847396+AbogeJr@users.noreply.github.com>
2022-02-03 23:08:18 +0300

    login/signup forms

--- myapp.py
+++ myapp.py
@@ -1,5 +1,11 @@
+from crypt import methods
 from flask import Flask, render_template
+
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'c968502a61da53471f4fd0fcb148cada'
 
 posts = [
     {
@@ -41,6 +47,17 @@ def hello_world():
 @app.route("/about")
 def about():
     return render_template('about.html', title="About")    
+
+@app.route("/registration")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title="Registration", form=form, methods=['GET', 'POST'])
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title="Login", form=form, methods=['GET', 'POST'])
+
 
 if __name__ == '__main__':
     app.run(debug=True)
